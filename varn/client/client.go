9131d001aee1ebf769caaff70fa2f3741afdb3d3
Sheel Rana <ranasheel2000@gmail.com>
2021-06-13 04:45:43 +0900

    Update client.go

--- varn/client/client.go
+++ varn/client/client.go
@@ -1,1 +1,14 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/ranasheel2000/GoLang/varn/proxy"
+)
+
+func main() {
+    // Get a greeting message and print it.
+    message := proxy.Hello("Gladys")
+    fmt.Println(message)
+}
 
