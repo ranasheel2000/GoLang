fecd3bc43cafa361721b908767a15c9b05723ad0
Sheel Rana <ranasheel2000@gmail.com>
2021-06-14 05:19:36 +0900

    Update proxy.go

--- varn/proxy/proxy.go
+++ varn/proxy/proxy.go
@@ -4,24 +4,35 @@ import (
     "fmt"
     "net/http"
     "time"
-    //"encoding/json"
     "io"
+    "flag"
 )
 
 func main() {
+    var host="localhost"
+    var port=8080
+    flag.IntVar(&port, "port", 8080, "specify port to use, defaults to 8080.")
+    flag.Parse()
     http.HandleFunc("/test", proxy)
-    http.ListenAndServe(":8019", nil)
+    addr := fmt.Sprintf("%s:%d", host, port)
+    http.ListenAndServe(addr, nil)
 }
 
 func proxy(w http.ResponseWriter, req *http.Request) {
     client := &http.Client{Timeout: 10 * time.Second}
     req, err := http.NewRequest("GET", "https://postman-echo.com/headers", nil)
-    req.Header.Add("key","the-secret-key")
-    resp,err:=client.Do(req)
-    output, err := io.ReadAll(resp.Body)
-    fmt.Fprintf(w, "%s",output)
     if err != nil {
         panic(err)
     }
+    req.Header.Add("key","the-secret-key")
+    resp,err:=client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    output, err := io.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Fprintf(w, "%s",output)
     defer resp.Body.Close()
 }
