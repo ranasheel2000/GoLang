fc96a316bf3bc49cd17576b94c66df6051cdceaf
Sheel Rana <ranasheel2000@gmail.com>
2021-06-14 05:20:09 +0900

    Update client.go

--- varn/client/client.go
+++ varn/client/client.go
@@ -1,14 +1,32 @@
 package main
-
 import (
+    "flag"
     "fmt"
-
-    "github.com/ranasheel2000/GoLang/varn/proxy"
+    "time"
+    "net/http"
+    "io"
 )
-
 func main() {
-    // Get a greeting message and print it.
-    message := proxy.Hello("Gladys")
-    fmt.Println(message)
-}
+    var host="http://localhost"
+    var port=8080
+    flag.IntVar(&port, "port", 8080, "specify port to use, defaults to 8080.")
+    flag.Parse()
+    addr := fmt.Sprintf("%s:%d/test", host, port)
 
+    client := &http.Client{Timeout: 10 * time.Second}
+    req, err := http.NewRequest("GET", addr, nil)
+    if err != nil {
+        panic(err)
+    }
+    resp,err:=client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    output, err := io.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(string(output))
+    defer resp.Body.Close()
+
+}
